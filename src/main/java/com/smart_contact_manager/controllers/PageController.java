fea2b03e4e2821c077f202ddab10758bef278cf4
mukullFromMac <mukulsavita0108@gmail.com>
2025-01-26 18:11:44 +0530

    Created Entities to handle signup form data and created the frontend for signup page, and edited the contact us frontend

--- src/main/java/com/smart_contact_manager/controllers/PageController.java
+++ src/main/java/com/smart_contact_manager/controllers/PageController.java
@@ -3,8 +3,6 @@ package com.smart_contact_manager.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 
